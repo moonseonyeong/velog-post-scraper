4aaf1c0eb34cf991b25102a61134f9b253635db3
moonseonyeong <seon_moon@naver.com>
2023-08-03 22:58:04 +0900

    getTotalPostCount 함수 추가

---
+++ src/scraper/getTotalPostCount.ts
@@ -0,0 +1,36 @@
+import { goToPage } from '../utils/browser';
+
+export const getTotalPostCount = async (URL: string) => {
+  const { page } = await goToPage(false, URL);
+
+  const postCountText = await page.evaluate(() => {
+    const postTagList = document.querySelectorAll('ul')[0].children;
+    const postTagListArr = Array.from(postTagList);
+    const countTag = postTagListArr.filter((li: HTMLLIElement) =>
+      li.outerText.includes('전체')
+    ) as HTMLLIElement[];
+
+    if (countTag.length === 0) {
+      return '게시글이 존재하지 않습니다.';
+    }
+
+    return countTag[0].innerText;
+  });
+
+  const count = extractNumberFromText(postCountText);
+
+  return count;
+};
+
+const extractNumberFromText = (text: string) => {
+  // 정규식 패턴: 괄호 안에 있는 숫자를 추출
+  const regex = /\((\d+)\)/;
+  const match = text.match(regex);
+
+  if (match && match[1]) {
+    const numberValue = parseInt(match[1]);
+    return numberValue;
+  }
+
+  return 0;
+};
