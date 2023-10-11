1c59e0d11074effa3d647abeee9491b9bb177ebc
moonseonyeong <seon_moon@naver.com>
2023-10-11 17:02:35 +0900

    tsconfig fix

--- src/scraper/index.ts
+++
@@ -1,6 +0,0 @@
-import { scrapeVelogPosts } from './scrapeVelogPosts';
-import { getTotalPostCount } from './getTotalPostCount';
-import { downloadVelogPosts } from './downloadVelogPosts';
-import { consoleUtils } from '../utils/console';
-
-export { scrapeVelogPosts, getTotalPostCount, downloadVelogPosts, consoleUtils };
