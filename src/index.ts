adabcd06fcc7ed611d6989e64b590b50149a1069
moonseonyeong <seon_moon@naver.com>
2023-10-11 16:51:35 +0900

    tsconfig.json fix

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import { scrapeVelogPosts } from './scraper/scrapeVelogPosts';
 import { getTotalPostCount } from './scraper/getTotalPostCount';
 import { downloadVelogPosts } from './scraper/downloadVelogPosts';
-import { consoleUtils } from 'utils/console';
+import { consoleUtils } from './utils/console';
 
 const add = (a: number, b: number) => {
   return a + b;
